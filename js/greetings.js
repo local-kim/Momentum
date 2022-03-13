7baa7c05afe9fc137899ca9c0c68c504729a2f54
김현지 <hyunji@Hyunzi-MacBook-Air.local>
2022-03-13 15:52:39 +0900

    Add greetings

--- js/greetings.js
+++ js/greetings.js
@@ -2,6 +2,8 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 const greeting = document.querySelector("#greeting");
+const todoForm = document.querySelector("#todo-form");
+const todoList = document.querySelector("#todo-list");
 
 const HIDDEN_CLASS = "hidden";
 const USERNAME_KEY = "username";
@@ -15,8 +17,25 @@ function onLoginSubmit(event){
 }
 
 function paintGreetings(username){
+  todoForm.classList.remove(HIDDEN_CLASS);
+  todoList.classList.remove(HIDDEN_CLASS);
   greeting.classList.remove(HIDDEN_CLASS);
-  greeting.innerText = `Hello ${username}`;
+
+  const date = new Date();
+  let greetingExpression = "Good ";
+
+  if(date.getHours() >= 6 && date.getHours() < 10){
+    greetingExpression += "morning, ";
+  } else if(date.getHours() >= 12 && date.getHours() < 17) {
+    greetingExpression += "afternoon, ";
+  } else if(date.getHours() >= 17 && date.getHours() < 21) {
+    greetingExpression += "evening, ";
+  } else if(date.getHours() >= 21 && date.getHours() < 24){
+    greetingExpression += "night, ";
+  } else {
+    greetingExpression = "Hello, ";
+  }
+  greeting.innerText = greetingExpression + username + "!";
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
